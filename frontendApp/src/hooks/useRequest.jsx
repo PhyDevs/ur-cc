f28d5a1f2e02be4738b333ea022623864fc4511b
phydevs <9onfod71@gmail.com>
2019-07-12 03:35:49 +0000

    frontend: add useRequest hook

---
+++ frontendApp/src/hooks/useRequest.jsx
@@ -0,0 +1,53 @@
+import React from 'react';
+import axios from 'axios';
+import API_PATH from '../utils/api_path';
+
+const types = {
+	SUBMIT_STARTED: 0,
+	SUBMIT_DONE: 1,
+};
+
+const requsetReducer = (state, action) => {
+	switch (action.type) {
+		case types.SUBMIT_STARTED:
+			return { ...state, loading: true };
+		case types.SUBMIT_DONE:
+			return { ...state, loading: false, ...action.payload };
+		default:
+			return state;
+	}
+};
+
+const useRequest = () => {
+	const [state, dispatch] = React.useReducer(requsetReducer, {
+		loading: false,
+		errors: null,
+	});
+
+	const send = async (route, data, method = 'post') => {
+		let response = { data: null };
+		try {
+			const headers = {
+				'Content-Type': 'application/json',
+			};
+
+			dispatch({ type: types.SUBMIT_STARTED });
+			response = await axios({
+				method,
+				url: `${API_PATH}/${route}`,
+				data,
+				headers,
+			});
+			dispatch({ type: types.SUBMIT_DONE, payload: { errors: null } });
+		} catch (err) {
+			const errors = err.response.status === 500 ? { message: 'Connection error' } : err.response.data;
+			dispatch({ type: types.SUBMIT_DONE, payload: { errors } });
+		}
+
+		return response;
+	};
+
+	return [state, send];
+};
+
+export default useRequest;
