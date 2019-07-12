4adb52df81f1c026a8d3747a96c7f6f8dea5f4e3
phydevs <9onfod71@gmail.com>
2019-07-12 03:36:59 +0000

    frontend: add AuthContext context

---
+++ frontendApp/src/contexts/AuthContext.jsx
@@ -0,0 +1,52 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const AuthContext = React.createContext();
+
+const AuthProvider = ({ children }) => {
+	let authenticated;
+	try {
+		const value = localStorage.getItem('token');
+		authenticated = !!value;
+	} catch {
+		authenticated = false;
+	}
+
+	const [isAuthenticated, setIsAuthenticated] = React.useState(authenticated);
+
+	const state = React.useMemo(
+		() => ({
+			isAuthenticated,
+			setIsAuthenticated,
+		}),
+		[isAuthenticated]
+	);
+
+	return <AuthContext.Provider value={state}>{children}</AuthContext.Provider>;
+};
+
+const useAuth = () => {
+	const context = React.useContext(AuthContext);
+	if (context === undefined) throw Error('useAuth must be used inside AuthProvider');
+
+	const { isAuthenticated, setIsAuthenticated } = context;
+	const authenticate = React.useCallback(
+		token => {
+			try {
+				localStorage.setItem('token', token);
+				setIsAuthenticated(true);
+			} catch {
+				setIsAuthenticated(false);
+			}
+		},
+		[setIsAuthenticated]
+	);
+
+	return [isAuthenticated, authenticate];
+};
+
+AuthProvider.propTypes = {
+	children: PropTypes.node.isRequired,
+};
+
+export { AuthProvider, useAuth };
