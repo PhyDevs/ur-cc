579015ac946c1e33acc0b20698430542f48d67b6
phydevs <9onfod71@gmail.com>
2019-07-13 06:09:09 +0000

    frontend: edit NearbyShopsList sorting shops by distance & fixing date difference

--- frontendApp/src/components/NearbyShopsList.jsx
+++ frontendApp/src/components/NearbyShopsList.jsx
@@ -3,6 +3,7 @@ import useFetch from '../hooks/useFetch';
 import useRequest from '../hooks/useRequest';
 import useLocalStorage from '../hooks/useLocalStorage';
 import ShopCard from './ShopCard';
+import getDistance from '../utils/get-distance';
 
 const NearbyShopsList = () => {
 	const [shops, setShops] = React.useState([]);
@@ -10,16 +11,31 @@ const NearbyShopsList = () => {
 	const [, send] = useRequest();
 	const [dislikedShops, setDislikedShops] = useLocalStorage('dis_shops', []);
 
+	const getNearbyShops = React.useCallback(() => {
+		const dislikedShopsToIntArr = dislikedShops.map(({ shop_id: shopId, disliked_date: dislikedDate }) => {
+			return (new Date() - new Date(dislikedDate)) / (1000 * 60 * 60) >= 2 ? null : shopId;
+		});
+
+		// filtring disliked shops & injecting distance property to sort shops
+		const nearbyShopsPromises = Object.values(data)
+			.filter(({ _id: id }) => dislikedShopsToIntArr.indexOf(id) < 0)
+			.map(async shop => {
+				const distance = await getDistance(shop.location.coordinates[1], shop.location.coordinates[0]);
+				return { ...shop, distance };
+			});
+
+		Promise.all(nearbyShopsPromises).then(nearbyShops => {
+			const sortedShops = nearbyShops.sort(({ distance: a }, { distance: b }) => a - b);
+			setShops(sortedShops);
+		});
+	}, [data, dislikedShops]);
+
 	// Side effect when shops are loaded or Shop is disliked
 	React.useEffect(() => {
 		if (data !== null) {
-			const dislikedShopsToIntArr = dislikedShops.map(({ shop_id: shopId, disliked_date: dislikedDate }) => {
-				return new Date().getHours() - new Date(dislikedDate).getHours() >= 2 ? null : shopId;
-			});
-			const nearbyShops = Object.values(data).filter(({ _id: id }) => dislikedShopsToIntArr.indexOf(id) < 0);
-			setShops(nearbyShops);
+			getNearbyShops();
 		}
-	}, [data, dislikedShops]);
+	}, [data, getNearbyShops]);
 
 	// Triggerd when Like Shop Button Clicked
 	const likeHandler = async id => {
@@ -40,7 +56,7 @@ const NearbyShopsList = () => {
 			 * }
 			 */
 			const outdatedDisliekShops = dislikedShops.map(({ shop_id: shopId, disliked_date: dislikedDate }) =>
-				new Date().getHours() - new Date(dislikedDate).getHours() >= 2 ? shopId : null
+				(new Date() - new Date(dislikedDate)) / (1000 * 60 * 60) >= 2 ? shopId : null
 			);
 			setDislikedShops(prevShops =>
 				prevShops
