caaed6a0354c91c1b9540c591061673f107c4418
phydevs <9onfod71@gmail.com>
2019-07-13 02:53:58 +0000

    frontend: add useFetch hook

---
+++ frontendApp/src/hooks/useFetch.jsx
@@ -0,0 +1,65 @@
+import React from 'react';
+import axios from 'axios';
+import { useAuth } from '../contexts/AuthContext';
+import API_PATH from '../utils/api_path';
+
+const types = {
+	SUBMIT_STARTED: 0,
+	SUBMIT_DONE: 1,
+};
+
+const getToken = () => {
+	try {
+		const token = localStorage.getItem('token');
+		return token;
+	} catch {
+		throw Error("localStorage isn't accessible");
+	}
+};
+
+const requsetReducer = (state, action) => {
+	switch (action.type) {
+		case types.SUBMIT_STARTED:
+			return { ...state, loading: true };
+		case types.SUBMIT_DONE:
+			return { ...state, loading: false, ...action.payload };
+		default:
+			return state;
+	}
+};
+
+const useFetch = route => {
+	const [, , logout] = useAuth();
+	const [state, dispatch] = React.useReducer(requsetReducer, {
+		loading: false,
+		data: null,
+	});
+
+	const get = React.useCallback(async () => {
+		let response = { data: null };
+		try {
+			const token = getToken();
+			const headers = {
+				'Content-Type': 'application/json',
+				Authorization: `Bearer ${token}`,
+			};
+
+			dispatch({ type: types.SUBMIT_STARTED });
+			response = await axios.get(`${API_PATH}/${route}`, { headers });
+			dispatch({ type: types.SUBMIT_DONE, payload: { data: response.data } });
+		} catch (err) {
+			dispatch({ type: types.SUBMIT_DONE, payload: { data: null } });
+			if (!err.response || err.response.status === 401) {
+				logout();
+			}
+		}
+	}, [logout, route]);
+
+	React.useEffect(() => {
+		get();
+	}, [get, route]);
+
+	return state;
+};
+
+export default useFetch;
